ef470ce6e5db6f3b6b608953d1c00fdbdac11210
Matthew Wilson <wilsonmatt219@gmail.com>
2019-01-28 23:23:13 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,5 +2,7 @@ var playlist = {
   Lucero: "nights like these"
   
 function updatePlaylist(playlist, artistName, songTitle) {
-  
+  playlist: 'playlist',
+  artistName: 'Lucero',
+  songTitle: 'night like these'
 }
